6637c4a80bc9437b9bdfd93265f66d43ec43202a
khaled71612000 <khaled71612000@gmail.com>
2024-06-19 00:46:17 +0300

    First

---
+++ Assets/ScoreUi.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ScoreUi : MonoBehaviour
+{
+    public RowUi rowUi;
+    public ScoreManager scoreManager;
+    void Start()
+    {
+        scoreManager.AddScore(new Score("khaled", 61));
+        scoreManager.AddScore(new Score("ahmed", 4));
+        scoreManager.AddScore(new Score("eahab", 65));
+        scoreManager.AddScore(new Score("osama", 66));
+        scoreManager.AddScore(new Score("hamdy", 62));
+
+        var scores = scoreManager.GetHighScores().ToArray();
+        for (int i = 0; i < scores.Length; i++)
+        {
+            var row = Instantiate(rowUi, transform).GetComponent<RowUi>();
+            row.rank.text = (i + 1).ToString();
+            row.name.text = scores[i].name;
+            row.score.text = scores[i].score.ToString();
+        }
+    }
+}
+
